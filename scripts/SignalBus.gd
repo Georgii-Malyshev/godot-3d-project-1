2f05a418a90a84c6fcca3d1eb60346f8484a37e7
Georgii Malyshev <terminal451@gmail.com>
2022-07-18 01:23:45 +0500

    Create a signal bus to be used globally
    Should help in decoupling nodes from each other and
    maintaining a managable project architecture by
    using a global observer that tracks (and lists!) all
    signal-based connections between nodes

---
+++ scripts/SignalBus.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+# A global signal bus. Should be added to the project's AutoLoad to be used 
+# as a singleton (techically it's won't be a "true" singleton because it can
+# still be instanced more than once by the developer; just don't instance 
+# it manually and it'll be okay)
+
+
+# Define any signal that you need here: 
+# signal ExampleSignal1
+# signal ExampleSignal2
+
+
+# Whenever a signal must be emitted, don't emit it from the node directly; 
+# use this bus instead. 
+
+# In the emitter node use the global signal bus to emit a signal: 
+
+# SignalBus.emit_signal("ExampleSignal1", self.whatever_you_need_to_pass_from_emitter)
+
+# In the node that needs to connect to this signal, again, use the global signal bus:
+
+# SignalBus.connect("ExampleSignal1", self, "<target_function>" 
+# Here, 'SignalBus' is the source node and 'self' is the target node
