1726df77df58e22bc261a325939d72977e34443b
Georgii Malyshev <terminal451@gmail.com>
2022-07-20 21:40:10 +0500

    Create level switcher (to be implemented), move playerPawn signal

--- game/Main.gd
+++ game/Main.gd
@@ -3,9 +3,6 @@ extends Node
 
 func _enter_tree():
 	SignalBus.connect("spawnPlayer", self, "_on_Player_spawn")
-
-
-func _ready():
 	SignalBus.connect("shootProjectile", self, "_on_Player_shoot")
 
 
