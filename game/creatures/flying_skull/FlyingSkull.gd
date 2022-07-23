b6a152fb675380f4f95731e65c7f7876c29dec7e
Georgii Malyshev <terminal451@gmail.com>
2022-07-24 01:23:59 +0500

    Implenent enemy pathfinding AI (chasing player over the NavMesh)

--- game/creatures/flying_skull/FlyingSkull.gd
+++ game/creatures/flying_skull/FlyingSkull.gd
@@ -4,21 +4,30 @@ extends KinematicBody
 # stats
 var max_health : int = 30
 var current_health : int = 30
-var moveSpeed : float = 1.0
+var movement_speed : float = 1.0
 var attackDamage : int = 5
 var attackRate : float = 1.0
 var attackDistance : float = 2.0
 
+# pathfinding
+var path = []
+var path_node = 0
+
 # components
+onready var nav : Node = get_parent()  # get navigation node  # TODO decouple from parent?
 # TODO get rid of absolute node path and don't use node that is higher up in the hierarchy
 onready var player : Node = get_node("/root/Main/Player")
-onready var timer : Timer = get_node("Timer")
+onready var timer1 : Timer = get_node("Timer")
+onready var timer2 : Timer = get_node("Timer2")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.set_wait_time(attackRate)
-	timer.start()
+	timer1.set_wait_time(attackRate)
+	timer1.start()
+	
+	timer2.start()
+	timer2.set_wait_time(2)
 
 
 func _on_Timer_timeout():
@@ -28,12 +37,26 @@ func _on_Timer_timeout():
 
 
 func _physics_process(delta):
+	if path_node < path.size():  # if current node isn't the last one on the path
+		var direction : Vector3 = (path[path_node] - global_transform.origin)
+		if direction.length() < 0.5:
+			path_node += 1  # use the next node next
+		else:
+			# move towards the current node along the direction vector
+			move_and_slide(direction.normalized() * movement_speed, Vector3.UP)
+"""
 	# calculate direction to the player
 	var dir = (player.translation - translation).normalized()
 	dir.y = 0
 	
 	# move towards the player
-	move_and_slide(dir * moveSpeed, Vector3.UP)
+	move_and_slide(dir * movement_speed, Vector3.UP)
+"""
+
+
+func update_path_to(target_pos):
+	path = nav.get_simple_path(global_transform.origin, target_pos)
+	path_node = 0
 
 
 func attack():
@@ -48,3 +71,7 @@ func take_damage(damage):
 
 func die():
 	queue_free()
+
+
+func _on_Timer2_timeout():  # TODO rename Timer and Timer2 to something explanatory
+	update_path_to(player.global_transform.origin)
