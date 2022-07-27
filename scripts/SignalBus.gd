9fd69d1b07f535d4ce5792a7cc9f397e531b59d3
Georgii Malyshev <terminal451@gmail.com>
2022-07-28 01:24:56 +0500

    Update SignalBus's comment
    Explain more clearly when to and when no to use it. Add indents
    to separate example code lines from text.

--- scripts/SignalBus.gd
+++ scripts/SignalBus.gd
@@ -7,22 +7,19 @@ extends Node
 # still be instanced more than once by the developer; just don't instance 
 # it manually and it'll be okay)
 
+# Whenever nodes must be connected via signal(s) in a less than extremely simple & clear manner
+# (i.e. anything less obvious than connecting a Timer's parent to that Timer's timeout via GUI),
+# don't emit signals from the node directly; use this bus instead. 
 
 # Define any signal that you need here: 
-# signal ExampleSignal1
-# signal ExampleSignal2
-
-
-# Whenever a signal must be emitted, don't emit it from the node directly; 
-# use this bus instead. 
+# 	signal ExampleSignal1
+# 	signal ExampleSignal2
 
 # In the emitter node use the global signal bus to emit a signal: 
-
-# SignalBus.emit_signal("ExampleSignal1", any, number, of, arguments, that, must, be, passed)
+# 	SignalBus.emit_signal("ExampleSignal1", any, number, of, arguments, that, must, be, passed)
 
 # In the node that needs to connect to this signal, again, use the global signal bus like so:
-
-# SignalBus.connect("ExampleSignal1", self, "<target_function>" 
+# 	SignalBus.connect("ExampleSignal1", self, "<target_function>" 
 # Here, 'SignalBus' is the source node and 'self' is the target node
 
 
