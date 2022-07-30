f14d00c128ca8f541b4b739eb42711959ffeb5f6
Georgii Malyshev <terminal451@gmail.com>
2022-07-31 01:10:28 +0500

    Fire a quick barrage of projectiles with 'Bone Barrage' spell

--- game/spells/BoneBarrage.gd
+++ game/spells/BoneBarrage.gd
@@ -1,8 +1,15 @@
 extends Node
 
+var projectiles_number: int = 4
 # TODO use specific projectile
 var projectile: PackedScene = preload("res://game/spells/AbstractProjectile.tscn")
-
+onready var CooldownTimer: Timer = $CooldownTimer
+onready var BarrageRateTimer: Timer = $BarrageRateTimer
 
 func cast(caster: NodePath, spatial_to_cast_in: Spatial) -> void:
-	SignalBus.emit_signal("spawn_projectile", caster, projectile, spatial_to_cast_in)
+	if CooldownTimer.is_stopped():
+		BarrageRateTimer.start()
+		for i in projectiles_number:
+			SignalBus.emit_signal("spawn_projectile", caster, projectile, spatial_to_cast_in)
+			yield(BarrageRateTimer,"timeout")
+		BarrageRateTimer.stop()
