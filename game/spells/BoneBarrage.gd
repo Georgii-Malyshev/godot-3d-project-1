5b8c1b374aaf168899eb311d91ac42e51d0cf051
Georgii Malyshev <terminal451@gmail.com>
2022-07-31 17:30:56 +0500

    Add a warmup timer to Bone Barrage spell

--- game/spells/BoneBarrage.gd
+++ game/spells/BoneBarrage.gd
@@ -2,11 +2,12 @@ extends Node
 
 # stats
 var projectiles_number: int = 4
-var cast_time: float = 0.4 setget set_cast_time, get_cast_time
+var cast_time: float = 0.8 setget set_cast_time, get_cast_time
 var cast_slowdown_modifier: float = 0.1 setget set_cast_slowdown_modifier, get_cast_slowdown_modifier
 
 # components
 var projectile: PackedScene = preload("res://game/spells/BoneBarrageProjectile.tscn")
+onready var WarmupTimer: Timer = $WarmupTimer
 onready var CooldownTimer: Timer = $CooldownTimer
 onready var BarrageRateTimer: Timer = $BarrageRateTimer
 
@@ -30,9 +31,12 @@ func cast(caster: NodePath, spatial_to_cast_in: Spatial) -> bool:
 	"""
 	Returns true if cast was successfull, false if cast failed
 	"""
-	# TODO add backfire, random spread and a "warm-up" timer
+	# TODO add backfire, random spread
 	if CooldownTimer.is_stopped():
 		CooldownTimer.start()
+		WarmupTimer.start()
+		yield(WarmupTimer,"timeout")
+		
 		BarrageRateTimer.start()
 		for i in projectiles_number:
 			SignalBus.emit_signal("spawn_projectile", caster, projectile, spatial_to_cast_in)
