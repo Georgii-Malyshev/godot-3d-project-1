2fa87b0ca02c68b04c69a4978f17b74e76adebd8
Georgii Malyshev <terminal451@gmail.com>
2022-07-31 10:00:01 +0500

    Rename&move behavior tree scripts to respective directory, delete unused ones

--- game/creatures/skeleton/agitated.gd
+++
@@ -1,6 +0,0 @@
-extends BTConditional
-
-
-func _pre_tick(agent: Node, _blackboard: Blackboard) -> void:
-	assert("agitated" in agent)
-	verified = agent.agitated
--- game/creatures/skeleton/move_to_players_last_seen_location.gd
+++
@@ -1,6 +0,0 @@
-extends BTLeaf
-
-func _tick(agent: Node, _blackboard: Blackboard) -> bool:
-	# TODO add asserts?
-	agent.move_to_player_last_seen_position()
-	return succeed()
