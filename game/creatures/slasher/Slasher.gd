bc888f914eddba7327a709ec8e78508620ea7ea8
Georgii Malyshev <terminal451@gmail.com>
2022-08-01 22:27:37 +0500

    Use setter, change comment

--- game/creatures/slasher/Slasher.gd
+++ game/creatures/slasher/Slasher.gd
@@ -1,9 +1,9 @@
 extends "res://game/creatures/abstract_enemy/AbstractEnemy.gd"
 
 
-# stats
 func _ready() -> void:
-	max_health = 40
-	current_health = 40
-	movement_speed = 5.0
+	# override inherited stats
+	max_health = 35
+	set_current_health(35)
+	movement_speed = 6.0
 	attack_range = 1.5
