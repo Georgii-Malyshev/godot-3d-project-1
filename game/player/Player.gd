0e1e60a10f11722dbb6182a7411f3d6a7e61bec2
Georgii Malyshev <terminal451@gmail.com>
2022-08-07 17:30:10 +0500

    Refactor Player script

--- game/player/Player.gd
+++ game/player/Player.gd
@@ -10,8 +10,8 @@ var health_regen_tick_time: float = 20.0
 var health_regen_threshold: int = (max_health * 0.20)
 var health_regen_per_tick: int = 1
 
-var mana_regen_delay_time: float = 5.0
-var mana_regen_tick_time: float = 5.0
+var mana_regen_delay_time: float = 4.5
+var mana_regen_tick_time: float = 0.5
 var mana_regen_threshold: int = (max_mana * 0.25)
 var mana_regen_per_tick: int = 1
 
@@ -20,11 +20,12 @@ var sneak_speed_modifier: float = 0.4
 var run_speed_modifier: float = 2.5
 
 # State
-var is_casting: bool = false
-var is_running: bool = false setget set_is_running, get_is_running
-var is_sneaking: bool = false setget set_is_sneaking, get_is_sneaking
-var current_health: int = max_health setget set_current_health, get_current_health
-var current_mana: int = max_mana setget set_current_mana, get_current_mana
+var current_health: int = max_health setget _set_current_health, get_current_health
+var current_mana: int = max_mana setget _set_current_mana, get_current_mana
+
+var is_running: bool = false setget _set_is_running, get_is_running
+var is_sneaking: bool = false setget _set_is_sneaking, get_is_sneaking
+var is_casting: bool = false setget _set_is_casting, get_is_casting
 
 # Camera look
 var min_look_angle: float = -65.0
@@ -32,19 +33,19 @@ var max_look_angle: float = 45.0
 var look_sensitivity: float = 0.60
 
 # Physics
-var gravity: float = 18.0
-var snap: Vector3 = Vector3.DOWN
-var default_speed_modifier: float = 1
-var speed_modifier: float = default_speed_modifier  # gets reset, don't change here
-var speed_modifier_input: float = default_speed_modifier  # gets reset, don't change here
+var _gravity: float = 18.0
+var _snap_vector: Vector3 = Vector3.DOWN
+var _default_speed_modifier: float = 1
+var _speed_modifier: float = _default_speed_modifier  # gets reset, don't change here
+var _speed_modifier_input: float = _default_speed_modifier  # gets reset, don't change here
 # vectors
-var velocity: Vector3 = Vector3.ZERO
-var mouse_delta: Vector2 = Vector2()
+var _velocity: Vector3 = Vector3.ZERO
+var _mouse_delta: Vector2 = Vector2()
 
 # Components
-onready var ground_detection_ray_cast: RayCast = $GroundDetectionRayCast
-onready var camera: Node = $FpsCamera
-onready var animation_player: AnimationPlayer = $FpsCamera/AnimationPlayer
+onready var _ground_detection_ray_cast: RayCast = $GroundDetectionRayCast
+onready var fps_camera: Node = $FpsCamera
+onready var fps_camera_animation_player: AnimationPlayer = $FpsCamera/AnimationPlayer
 
 onready var health_regen_delay_timer: Timer = $HealthRegenDelayTimer
 onready var health_regen_tick_timer: Timer = $HealthRegenTickTimer
@@ -58,11 +59,11 @@ var spell: Node = preload("res://game/spells/BoneBarrage.tscn").instance()
 # (spawn projectiles from one spatial, cast line-of-sight ray from another etc.)
 onready var cast_spatial: Spatial = $FpsCamera/SpellcastingRightArm/ProjectileSpawnPoint
 #onready var cast_spatial: Spatial = $FpsCamera/CastSpellPoint
-onready var cast_transform: Transform setget set_cast_transform, get_cast_transform
+onready var cast_transform: Transform setget _set_cast_transform, get_cast_transform
 onready var cast_spell_timer: Timer = $CastSpellTimer
 
 
-func set_is_running(value: bool) -> void:
+func _set_is_running(value: bool) -> void:
 	if value:
 		is_sneaking = false
 		is_running = true
@@ -74,7 +75,7 @@ func get_is_running() -> bool:
 	return is_running
 
 
-func set_is_sneaking(value: bool) -> void:
+func _set_is_sneaking(value: bool) -> void:
 	if value:
 		if not is_running:
 			is_sneaking = true
@@ -88,9 +89,15 @@ func get_is_sneaking() -> bool:
 	return is_sneaking
 
 
-func set_current_health(value: int) -> void:
-	if value < current_health:
-		health_regen_delay_timer.start()
+func _set_is_casting(_value: bool) -> void:
+	print("Attempt to set is_casting with setter, ignoring")
+
+
+func get_is_casting() -> bool:
+	return is_casting
+
+
+func _set_current_health(value: int) -> void:
 	# warning-ignore:narrowing_conversion
 	current_health = clamp(value, 0, max_health)
 
@@ -99,9 +106,7 @@ func get_current_health() -> int:
 	return current_health
 
 
-func set_current_mana(value: int) -> void:
-	if value < current_mana:
-		mana_regen_delay_timer.start()
+func _set_current_mana(value: int) -> void:
 	# warning-ignore:narrowing_conversion
 	current_mana = clamp(value, 0, max_mana)
 
@@ -110,8 +115,8 @@ func get_current_mana() -> int:
 	return current_mana
 
 
-func set_cast_transform(_value: Transform) -> void:
-	print("Attempt to set cast_transform, ignoring")
+func _set_cast_transform(value: Transform) -> void:
+	cast_transform = value
 
 
 func get_cast_transform() -> Transform:
@@ -135,8 +140,8 @@ func _physics_process (delta):
 	
 	# Player movement
 	
-	velocity.x = 0
-	velocity.z = 0
+	_velocity.x = 0
+	_velocity.z = 0
 	_reset_speed_modifier_input()
 	is_running = false
 	is_sneaking = false
@@ -154,10 +159,10 @@ func _physics_process (delta):
 	
 	if Input.is_action_pressed("run"):
 		is_running = true
-		speed_modifier_input = run_speed_modifier
+		_speed_modifier_input = run_speed_modifier
 	elif Input.is_action_pressed("sneak"):
 		is_sneaking = true
-		speed_modifier_input = sneak_speed_modifier
+		_speed_modifier_input = sneak_speed_modifier
 	
 	# normalize diagonal movement speed
 	input = input.normalized()
@@ -167,32 +172,32 @@ func _physics_process (delta):
 	var right = global_transform.basis.x
 	
 	# set player movement velocity
-	velocity.z = (forward * input.y + right * input.x).z * move_speed * speed_modifier_input * speed_modifier
-	velocity.x = (forward * input.y + right * input.x).x * move_speed * speed_modifier_input * speed_modifier
+	_velocity.z = (forward * input.y + right * input.x).z * move_speed * _speed_modifier_input * _speed_modifier
+	_velocity.x = (forward * input.y + right * input.x).x * move_speed * _speed_modifier_input * _speed_modifier
 	
-	# apply gravity
-	velocity.y -= gravity * delta
+	# apply _gravity
+	_velocity.y -= _gravity * delta
 	
 	# move the player, snap to the ground, stop on slopes
-	var velocity_intermediate := move_and_slide_with_snap(
-		velocity, snap, Vector3.UP, true, 4, deg2rad(45)
+	var _velocity_intermediate := move_and_slide_with_snap(
+		_velocity, _snap_vector, Vector3.UP, true, 4, deg2rad(45)
 	)
 	
 	if is_on_wall():
-		ground_detection_ray_cast.set_enabled(true)
-		ground_detection_ray_cast.force_raycast_update()
-		if not ground_detection_ray_cast.is_colliding():
-			velocity_intermediate.y = min(0, velocity.y)
+		_ground_detection_ray_cast.set_enabled(true)
+		_ground_detection_ray_cast.force_raycast_update()
+		if not _ground_detection_ray_cast.is_colliding():
+			_velocity_intermediate.y = min(0, _velocity.y)
 	
-	velocity = velocity_intermediate
+	_velocity = _velocity_intermediate
 	
-	if (velocity.x != 0) or (velocity.z != 0):
+	if (_velocity.x != 0) or (_velocity.z != 0):
 		if is_running:
-			animation_player.play("HeadBobRunning")
+			fps_camera_animation_player.play("HeadBobRunning")
 		elif is_sneaking:
-			animation_player.play("HeadBobSneaking")
+			fps_camera_animation_player.play("HeadBobSneaking")
 		else:
-			animation_player.play("HeadBob")
+			fps_camera_animation_player.play("HeadBob")
 	
 	# Passive health & mana regen
 	_try_health_regen()
@@ -204,27 +209,27 @@ func _input(event):
 	# FPS camera movement
 	if event is InputEventMouseMotion:
 		# get direction and length that the mouse moved
-		mouse_delta = event.relative
+		_mouse_delta = event.relative
 		# rotate camera along X axis
-		camera.rotation_degrees -= Vector3(rad2deg(mouse_delta.y), 0, 0) * look_sensitivity * get_process_delta_time()
+		fps_camera.rotation_degrees -= Vector3(rad2deg(_mouse_delta.y), 0, 0) * look_sensitivity * get_process_delta_time()
 		# clamp vertical camera rotation
-		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, min_look_angle, max_look_angle)
+		fps_camera.rotation_degrees.x = clamp(fps_camera.rotation_degrees.x, min_look_angle, max_look_angle)
 		# rotate player along Y axis
-		rotation_degrees -= Vector3(0, rad2deg(mouse_delta.x), 0) * look_sensitivity * get_process_delta_time()
+		rotation_degrees -= Vector3(0, rad2deg(_mouse_delta.x), 0) * look_sensitivity * get_process_delta_time()
 		# reset the mouse delta vector
-		mouse_delta = Vector2()
+		_mouse_delta = Vector2()
 	
 	# Actions
 	if Input.is_action_just_pressed("cast_spell"):
-		cast_spell()
+		_cast_spell()
 
 
 func _reset_speed_modifier():
-	speed_modifier = default_speed_modifier
+	_speed_modifier = _default_speed_modifier
 
 
 func _reset_speed_modifier_input():
-	speed_modifier_input = default_speed_modifier
+	_speed_modifier_input = _default_speed_modifier
 
 func _try_health_regen() -> void:
 	# Should be called every physics step to work correctly
@@ -268,24 +273,23 @@ func _on_CastSpellTimer_timeout():
 
 
 func lose_health(amount):
-	# TODO move timer logic from setter here
-	set_current_health(current_health - amount)
-	
+	health_regen_delay_timer.start()
+	_set_current_health(current_health - amount)
 	if current_health <= 0:
 		_die()
 
 
 func lose_mana(amount):
-	# TODO move timer logic from setter here
-	set_current_mana(current_mana - amount)
+	mana_regen_delay_timer.start()
+	_set_current_mana(current_mana - amount)
 
 
 func restore_health(amount):
-	set_current_health(current_health + amount)
+	_set_current_health(current_health + amount)
 
 
 func restore_mana(amount):
-	set_current_mana(current_mana + amount)
+	_set_current_mana(current_mana + amount)
 
 
 func take_damage(damage):
@@ -296,13 +300,13 @@ func _die():
 	print("Player dies!")  # TODO implement player death mechanic
 
 
-func cast_spell():
+func _cast_spell():
 	var spell_mana_cost: int = spell.get_mana_cost()
 	if (not is_casting) and (current_mana >= spell_mana_cost):
 		# try to cast spell
 		if not spell.get_is_on_cooldown():
 			spell.cast(self.get_path())
 			is_casting = true
-			set_current_mana(current_mana - spell_mana_cost)
+			lose_mana(spell_mana_cost)
 			cast_spell_timer.start(spell.get_cast_time())
-			speed_modifier = speed_modifier * spell.get_cast_slowdown_modifier()
+			_speed_modifier = _speed_modifier * spell.get_cast_slowdown_modifier()
