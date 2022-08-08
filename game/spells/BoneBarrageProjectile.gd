51d0d5de2813d9d1bd259763b8b7d86c6825a364
Georgii Malyshev <terminal451@gmail.com>
2022-08-08 20:50:23 +0500

    Increase Bone Barrage's spread and reduce it's max distance

--- game/spells/BoneBarrageProjectile.gd
+++ game/spells/BoneBarrageProjectile.gd
@@ -2,8 +2,9 @@ extends "res://game/spells/AbstractProjectile.gd"
 
 var max_pierce: int = 3
 var pierce_counter: int = 0
+var spread: float = 0.023
 
-# for random spread
+# for physics of random spread
 var phi_for_x: float
 var phi_for_y: float
 
@@ -11,19 +12,19 @@ var phi_for_y: float
 func _ready():
 	velocity = 25.0
 	damage = 25
-	projectile_expire_time = 2.5
+	projectile_expire_time = 0.56
 	DestroyTimer.set_wait_time(projectile_expire_time)
 	DestroyTimer.start()
 	
 	# rotate projectile for random spread
-	phi_for_x = rand_range(-0.01, 0.01)
-	phi_for_y = rand_range(-0.01, 0.01)
+	
+	phi_for_x = rand_range(-spread, spread)
+	phi_for_y = rand_range(-spread, spread)
 	global_transform.rotated(Vector3(1, 0, 0), phi_for_x)  # rotate projectile on X axis
 	global_transform.rotated(Vector3(1, 0, 0), phi_for_y)  # rotate projectile on Y axis
 
 
 func _process(delta):
-	
 	# make sure that inheriting projectiles have started DestroyTimer so they don't travel indefinitely
 	assert(not DestroyTimer.is_stopped())
 	
